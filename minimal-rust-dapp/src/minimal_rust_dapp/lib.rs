d109dbeef6c7657e7dd018115fcf08bbfba3a7e0
cj-dfinity <98783994+cj-dfinity@users.noreply.github.com>
2022-02-15 07:37:48 -0800

    Update lib.rs

--- minimal-rust-dapp/src/minimal_rust_dapp/lib.rs
+++ minimal-rust-dapp/src/minimal_rust_dapp/lib.rs
@@ -1,4 +1,21 @@
+static mut COUNTER: u64 = 0;
+
+#[ic_cdk_macros::update]
+fn increment() -> u64 {
+    unsafe {
+        COUNTER += 1;
+        COUNTER
+    }
+}
+
 #[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get() -> u64 {
+    unsafe { COUNTER }
+}
+
+#[ic_cdk_macros::update]
+fn set(input: u64) -> () {
+    unsafe {
+        COUNTER = input;
+    }
 }
